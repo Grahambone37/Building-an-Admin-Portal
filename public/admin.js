135cb8b93e9a6f9dfda1a7cb430eafd68fd1e5bd
Grahambone37 <electricgrunt37@gmail.com>
2022-12-01 00:04:11 -0800

    finally got the titles by themselves

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,11 @@
 
 // Your Code Here
+function books(){
+    let bookList = document.querySelector('#book-list');
+    let titles = [];
+    fetch('http://localhost:3001/listBooks')
+        .then((response) => response.json())
+        .then(book => titles = book.map(obj => obj.title))
+        .then(newArr => console.log(newArr))
+}
+books()
